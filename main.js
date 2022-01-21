9bf2a1e99f357ac6e0fe8e4ea3232ec8d065f282
Julia G <informacje.zatajone@gmail.com>
2022-01-21 11:00:11 +0100

    Init commit, loops and variables tasks

---
+++ main.js
@@ -0,0 +1,226 @@
+// 1
+
+// let Napis = 5;
+// const Napisy = Napis - 3;
+
+// Napis = Napis -3;
+// 2
+// let result = "Hello";
+// result = result + " World"  <--- checked
+
+// 3
+let a = 5;
+let b = 10;
+console.log(a*b, a/b, a+b, a-b)
+// 4
+// let inc = 10;
+// inc = inc + 1
+    inc++
+    inc += 1
+// let number = 1;
+// number = number + "1"
+
+// 5
+// let number = 10;
+// let number2 = 3;
+
+// 6
+
+// let napis10 = 10;
+// napis10 = napis10 + 15
+
+// 7
+// let test = true
+// if (test == true) {
+//  console.log("Zmienna test jest równa true")
+// }
+// 8
+// let world = "Świat"
+// if(world == "Świat"){
+//     console.log("Zmienna world zawiera wartość świat")
+// }
+//9
+
+// let word = "słowo"
+// if(word == babcia){
+//     console.log("Zmienna word jest równa babcia")
+// }
+// else{
+//     console.log("Zmienna word nie jest równa babcia")
+// }
+//10
+// let zapasPaliwa = 10;
+// if(zapasPaliwa >4 ){
+//     console.log("Masz wystarczający zapas paliwa")
+// }
+
+//11
+// let zapasPaliwa2 = 2
+// if(4<zapasPaliwa2<=10){
+//     console.log("Masz wystarczający zapas paliwa, ale nie za dużo")
+// } 
+
+//12
+// let zapasPaliwa3 = 10;
+// if(4<zapasPaliwa3 && zapasPaliwa3<10){
+//     console.log("Masz wystarczający zapas paliwa,ale nie za dużo")
+// }
+// else{
+//     console.log("Masz dużo paliwa!!!")
+// }
+
+//13
+// let pulap = 1000;
+// if(pulap>10000){
+//     console.log("Pułap przelotny")
+// }
+// else if(pulap<10000 && pulap>=8000){
+//     console.log("duży pułap")
+// }
+// else if(pulap<8000 && pulap>2000){
+// console.log("niski pułap")
+// }
+// else{
+//     console.log("pułap podejścia")
+// }
+//14
+// let przebiegniete = 7;
+// if(przebiegniete>15){
+//     console.log("Przebiegłeś dużo kilometrów")
+// }
+// else if(przebiegniete<15 && przebiegniete>12){
+//     console.log("dobrze Ci idzie!")
+// }
+// else{
+//     console.log("Z czasem będzie lepiej")
+// }
+//15
+// let zapasPaliwa3 = 5;
+// if(zapasPaliwa3>4 && zapasPaliwa3<10){
+//     console.log("Masz wystarczający zapas paliwa, ale nie za dużo")
+// }
+// else if(zapasPaliwa3>=10){
+//     console.log("Masz dużo paliwa!!!")
+// }
+// else{
+//     console.log("bida")
+// }
+
+//16
+// let garnek = 25*4;
+// let wlewamy = 12;
+// if(wlewamy<=garnek){
+//     console.log("zmieści się")
+// }
+// else{
+//     console.log("co ty odwalasz?")
+// }
+
+//17
+// let szerokośćsamolotu = 10;
+// let  wysokośćsamolotu = 5;
+// let długośćsamolotu = 30;
+// let szerokośćładunku = 6;
+// let wysokośćładunku = 6;
+// let długośćładunku = 10;
+// let objętośćsamolotu = wysokośćsamolotu*długośćsamolotu*szerokośćsamolotu;
+// let objętośćładunku = wysokośćładunku*długośćładunku*szerokośćładunku;
+// if(szerokośćsamolotu>szerokośćładunku && wysokośćsamolotu>wysokośćładunku && długośćsamolotu>długośćładunku && objętośćsamolotu>objętośćładunku){
+//     console.log("zmieści się panie kierowniku")
+// }
+// else{
+//     console.log("nie da rady")
+// }
+
+// if(wysokośćsamolotu>wysokośćładunku && objętośćsamolotu>objętośćładunku){
+//     console.log("Ok")
+// }
+// else{
+//     console.log("nie ok")
+// }
+
+//18
+// let zmienna = 12;
+// if(12||3&&12||2){
+//     console.log("Jest podzielne przez 2 i 3")
+// }
+// else{
+//     console.log("Nie są to liczby podzielne przez 2 i 3")
+// }
+
+//19
+// let liczba = 111;
+// if(liczba%11 !=0){
+//     console.log("liczba nie podzielna");
+// }
+// else{
+//     console.log("podzielna")
+// }
+
+//20 - Pętle
+// for (let i = 0; i < 6; i++) {
+//   console.log(i);
+// }
+
+//21
+
+// for (let i = 1; i < 10; i+=2) {
+//       console.log(i);
+//     }
+
+//22
+// for(let i = 2; i <10; i+=2) {
+//     console.log(i);
+// }
+
+//23
+// for(i = 5; i <= 25; i++) {
+//     if(i%3 === 0 ){
+//         console.log(i)
+//     }
+// }
+
+//205
+// for(i = 10; i<125; i++){
+//     if(i%5 === 0){
+//         console.log(i)
+//     }
+
+// }
+
+//206
+// for(i = 10; i<125; i++){
+//     if(i%5 === 0 && i%3 === 0){
+//         console.log(i)
+//     }
+// }
+
+//207
+// const tablica = [1,2,10]
+
+//208
+// const tablica2 = [1,10,12]
+
+// for(i=0; i<tablica2.length; i++){
+//     tablica2[i] = tablica2[i] +5;
+// }
+
+// console.log(tablica2)
+
+//209
+// const tablica3 = [1,10,12]
+// for(i=0; i<tablica3.length; i++){
+//     tablica3[i] = tablica3[i]*10;
+// 
+
+//210
+// const mieszana = [1, "Napis",true]
+
+//211
+const tablica4 = [1,10,12]
+for(let i = 0; i<tablica4.length; i++){
+    console.log()
+}
+
+
+    
